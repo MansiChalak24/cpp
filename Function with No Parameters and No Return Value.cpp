f97bd94de1986575506162ac14f532837e9e95d1
MansiChalak24 <mansi.chalak24@sanjivani.edu.in>
2024-11-17 19:36:22 +0530

    Add files via upload
    // function with no parameters and no return value
    
    The program starts by employing the main() function.
    Inside main(), the greet() function is called.
    When the greet() function is called:
    It writes the message "Hello, welcome to C++ programming!" to the console.
    The program then returns to the main() function after the function call completes.
    The last function, main(), returns 0 to indicate successful termination of the program.

---
+++ Function with No Parameters and No Return Value.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+// Function definition with no parameters and no return value
+void greet() {
+cout << "Hello, welcome to C++ programming!" << endl;
+}
+
+int main() {
+// Function call
+greet();
+return 0;
+}
